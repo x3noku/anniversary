d596f12770e6b0ec5b58b8da319ba02077261b23
x3noku <x3noku@gmail.com>
2025-03-25 03:59:28 +0300

    inital commit

---
+++ src/env.js
@@ -0,0 +1,17 @@
+import { createEnv } from '@t3-oss/env-nextjs';
+import { z } from 'zod';
+
+export const env = createEnv({
+    server: {
+        NODE_ENV: z.enum(['development', 'test', 'production']),
+    },
+    client: {
+        // NEXT_PUBLIC_CLIENTVAR: z.string(),
+    },
+    runtimeEnv: {
+        NODE_ENV: process.env.NODE_ENV,
+        // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    },
+    skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+    emptyStringAsUndefined: true,
+});
