0cb7c6f2e80ae5942243e0bc4b225917e4513f94
x3noku <x3noku@gmail.com>
2025-04-10 03:14:19 +0600

    chore: add debug mode

--- src/features/CountdownProvider/lib/useIsShown.ts
+++ src/features/CountdownProvider/lib/useIsShown.ts
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
+import { useLocalStorage } from '~/shared/lib/hooks';
 
 export const useIsShown = (countdown: number) => {
+    const [debug] = useLocalStorage<boolean>('debug', false);
     const [prevCountdown, setPrevCountdown] = useState(countdown);
 
     useEffect(() => setPrevCountdown(countdown), [countdown]);
 
-    return prevCountdown > 0;
+    return !debug && prevCountdown > 0;
 };
--- src/shared/lib/hooks/index.ts
+++ src/shared/lib/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useIsClient';
+export * from './useLocalStorage';
 export * from './useMount';
 export * from './useToggle';
