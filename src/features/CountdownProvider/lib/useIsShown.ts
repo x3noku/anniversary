3cf405a405ffd2b16688f49f2b578e1ba85991f6
x3noku <x3noku@gmail.com>
2025-04-11 22:13:50 +0600

    chore: add debug mode from url

--- src/features/CountdownProvider/lib/useIsShown.ts
+++ src/features/CountdownProvider/lib/useIsShown.ts
@@ -1,11 +1,14 @@
+import { useSearchParams } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import { useLocalStorage } from '~/shared/lib/hooks';
 
 export const useIsShown = (countdown: number) => {
+    const searchParams = useSearchParams();
+
     const [debug] = useLocalStorage<boolean>('debug', false);
     const [prevCountdown, setPrevCountdown] = useState(countdown);
 
     useEffect(() => setPrevCountdown(countdown), [countdown]);
 
-    return !debug && prevCountdown > 0;
+    return !debug && !searchParams.has('debug') && prevCountdown > 0;
 };
