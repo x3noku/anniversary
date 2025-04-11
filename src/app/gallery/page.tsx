e811e7bfcfa17e965d97e7f4e84237fd7813e0ee
x3noku <x3noku@gmail.com>
2025-04-11 21:57:00 +0600

    fix: fix gallery on ios

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,8 +1,4 @@
-import { formatDate } from 'date-fns/format';
 import * as motion from 'motion/react-client';
-import Image from 'next/image';
-import { env } from '~/env';
-import redTile from '~/public/assets/red-tile.png';
 import type { SearchParams } from '~/shared/lib/types';
 import { Redirect } from '~/shared/ui/redirect';
 import { Gallery } from '~/widgets/Gallery';
@@ -16,27 +12,14 @@ export default async ({ searchParams }: SearchParams<{ transition?: string }>) =
                 <>
                     <Redirect href={'/gallery'} delay={1000} />
                     <motion.div
-                        className={'pointer-events-none absolute z-200 h-lvh w-screen select-none overflow-hidden'}
+                        className={'pointer-events-none absolute z-200 h-lvh w-screen bg-[#FE0000]'}
                         initial={{ opacity: 1 }}
                         animate={{ opacity: 0 }}
                         transition={{ delay: 0.5, duration: 0.5 }}
-                    >
-                        <Image src={redTile.src} className={'scale-150 object-cover'} fill priority alt={''} />
-                    </motion.div>
+                    />
                 </>
             )}
-            {transition === undefined && (
-                <main className={'flex h-lvh flex-col items-stretch justify-start gap-y-8 overflow-auto px-8 py-24'}>
-                    <span className={'text-center'}>
-                        <h1 className={'font-heading font-semibold text-5xl'}>Anniversary</h1>
-                        <h3 className={'mt-4 text-lg'}>
-                            Celebrating our love on {formatDate(env.NEXT_PUBLIC_TARGET_DATE, 'MMMM d, y')}
-                        </h3>
-                        <h5 className={'text-stone-500 text-xs leading-none'}>Almost March 26</h5>
-                    </span>
-                    <Gallery />
-                </main>
-            )}
+            {transition === undefined && <Gallery />}
         </>
     );
 };
--- src/widgets/Gallery/ui/Gallery.tsx
+++ src/widgets/Gallery/ui/Gallery.tsx
@@ -1,17 +1,50 @@
 'use client';
 
+import { formatDate } from 'date-fns/format';
+import { type ComponentRef, useEffect, useRef, useState } from 'react';
+import { env } from '~/env';
+import { cn } from '~/lib/utils';
 import { useItems } from '../config/items';
 import { Card } from './Card';
 
 const Gallery: React.FC = () => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    const mainRef = useRef<ComponentRef<'main'>>(null);
+    const ulRef = useRef<ComponentRef<'ul'>>(null);
+
     const items = useItems();
 
+    useEffect(() => {
+        if (!isMounted) return setIsMounted(true);
+
+        mainRef.current?.scrollTo(0, 500);
+        new Promise(resolve => setTimeout(resolve, 100)).finally(() => {
+            mainRef.current?.scrollTo(0, 0);
+        });
+    }, [isMounted]);
+
     return (
-        <ul className={'flex flex-col items-center'}>
-            {items.map((item, index) => (
-                <Card key={index.toString()} {...item} />
-            ))}
-        </ul>
+        <main
+            ref={mainRef}
+            className={cn(
+                'flex h-lvh flex-col items-stretch justify-start gap-y-8 overflow-auto px-8 pt-24 pb-32',
+                'fade-in animate-in fill-mode-backwards delay-150 duration-500',
+            )}
+        >
+            <span className={'text-center'}>
+                <h1 className={'font-heading font-semibold text-5xl'}>Anniversary</h1>
+                <h3 className={'mt-4 text-lg'}>
+                    Celebrating our love on {formatDate(env.NEXT_PUBLIC_TARGET_DATE, 'MMMM d, y')}
+                </h3>
+                <h5 className={'text-stone-500 text-xs leading-none'}>Almost March 26</h5>
+            </span>
+            <ul ref={ulRef} className={'flex flex-col items-center'}>
+                {items.map((item, index) => (
+                    <Card key={index.toString()} {...item} />
+                ))}
+            </ul>
+        </main>
     );
 };
 Gallery.displayName = 'Gallery';
